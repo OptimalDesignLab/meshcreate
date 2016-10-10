5363a71673410703e43688b8ba1e43fccaab0c2e
Jared Crean <jcrean01@gmail.com>
2016-10-10 14:00:17 -0400

    remove unneeded options from config.sh

--- config.sh
+++ config.sh
@@ -11,12 +11,5 @@ cd ./build
 cmake .. \
   -DSCOREC_PREFIX=$SCOREC_PREFIX \
   -DCMAKE_CXX_COMPILER="mpicxx" \
-  -DCMAKE_C_FLAGS="-O2 -g -Wall" \
   -DCMAKE_CXX_FLAGS="-O2 -g -Wall -std=c++11" \
-  -DENABLE_THREADS=OFF \
   -DCMAKE_INSTALL_PREFIX=$installdir
-#  -DIS_TESTING=True
-#  -DENABLE_ZOLTAN=
-
-
-#  -DCMAKE_C_COMPILER="mpicc" \
